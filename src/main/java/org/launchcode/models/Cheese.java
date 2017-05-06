e37431da638d38b280da490a02f93106b94b6583
Carly Langlois <cj.langlois.cl@gmail.com>
2017-05-06 16:41:07 -0500

    class 5 prep

---
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -0,0 +1,26 @@
+package org.launchcode.models;
+
+/**
+ * Created by carlylanglois on 5/6/17.
+ */
+public class Cheese {
+
+    private String name;
+    private String description;
+
+    //get and set name
+    public String getName() {
+        return name;
+    }
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    //get and set description
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String aDescription) {
+        description = aDescription;
+    }
+}
